1b22121184a1da27334e24fe9e80e33df56a1576
Galih Suprayogo <suprayogogalih@gmail.com>
2024-02-20 16:44:23 +0700

    Navbar is okay

--- app/components/NavList/index.tsx
+++ app/components/NavList/index.tsx
@@ -6,7 +6,7 @@ const NavList = (props: NavListProps) => {
     <ul className='my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6'>
       <Link href={props.href} target={props.target}>
         <li>
-          <span className='font-sans text-lg font-bold uppercase text-white lg:text-xl'>
+          <span className='font-sans text-base font-bold uppercase text-white lg:text-lg'>
             {props.title}
           </span>
         </li>
--- app/components/NavWrapper/index.tsx
+++ app/components/NavWrapper/index.tsx
@@ -3,13 +3,39 @@ import { NavList } from 'components'
 import { NavListProps } from '@/types'
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import { XMarkIcon } from '@heroicons/react/20/solid'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const ButtonDonateLanguage = () => (
+  <div className='hidden flex-col space-y-3 bg-none lg:flex'>
+    <span className='text-right text-base font-semibold text-white'>{`EN | ID`}</span>
+    <span className='bg-primary-900 px-6 py-1 text-lg font-bold text-white'>Donate</span>
+  </div>
+)
 
 const NavWrapper = (props: {
   data?: NavListProps[]
   openNavbar?: boolean
   setOpenNavbar?: (isOpen: boolean) => void
 }) => (
-  <div className='relative flex h-fit w-full flex-row items-start justify-end py-2 lg:justify-between'>
+  <div
+    className={`relative flex h-fit w-full flex-row ${props.openNavbar ? 'items-start' : 'items-center'} justify-between py-2`}
+  >
+    <Link href='/' target='_top' className={`${props.openNavbar ? 'hidden' : 'block'}`}>
+      <Image
+        src='/logo.png'
+        alt='navbar-logo'
+        placeholder='blur'
+        blurDataURL='/logo.png'
+        height={0}
+        width={0}
+        sizes='100vw'
+        style={{
+          objectFit: 'contain',
+        }}
+        className='h-auto w-[15rem] bg-none lg:w-[22rem]'
+      />
+    </Link>
     <div className='hidden w-full flex-row items-center justify-around lg:flex'>
       {props.data?.map((item, index) => (
         <NavList key={index} title={item.title} target={item.target} href={item.href} />
@@ -36,6 +62,7 @@ const NavWrapper = (props: {
         {createElement(XMarkIcon, { className: 'h-6 w-6 text-white', strokeWidth: 4 })}
       </div>
     </React.Fragment>
+    <ButtonDonateLanguage />
   </div>
 )
 
