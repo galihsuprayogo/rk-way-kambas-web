6ca96f4a8d54f23ec6c0aeb5373eb5c196c61e30
Galih Suprayogo <suprayogogalih@gmail.com>
2024-02-21 08:26:12 +0700

    Resolve show/hide donate button

--- app/components/NavWrapper/index.tsx
+++ app/components/NavWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React, { createElement } from 'react'
+import React, { HTMLProps, createElement } from 'react'
 import { NavList } from 'components'
 import { NavListProps } from '@/types'
 import { Bars3Icon } from '@heroicons/react/20/solid'
@@ -6,9 +6,12 @@ import { XMarkIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const ButtonDonateLanguage = () => (
-  <div className='hidden flex-col space-y-3 bg-none lg:flex'>
-    <span className='text-right text-base font-semibold text-white'>{`EN | ID`}</span>
+const ButtonDonateLanguage = (props: {
+  className?: HTMLProps<HTMLElement>['className']
+  textAlign?: 'text-left' | 'text-right'
+}) => (
+  <div className={`${props.className} flex-col space-y-3 bg-none`}>
+    <span className={`${props.textAlign} text-base font-semibold text-white`}>{`EN | ID`}</span>
     <span className='bg-primary-900 px-6 py-1 text-lg font-bold text-white'>Donate</span>
   </div>
 )
@@ -33,7 +36,7 @@ const NavWrapper = (props: {
         style={{
           objectFit: 'contain',
         }}
-        className='h-auto w-[15rem] bg-none lg:w-[22rem]'
+        className='h-auto w-[12rem] bg-none md:w-[15rem] lg:w-[22rem]'
       />
     </Link>
     <div className='hidden w-full flex-row items-center justify-around lg:flex'>
@@ -42,11 +45,12 @@ const NavWrapper = (props: {
       ))}
     </div>
     <div
-      className={`${props.openNavbar ? 'block' : 'hidden'} sticky inset-0 z-40 mt-10 flex flex-grow flex-col items-start justify-center space-y-5 overflow-y-scroll rounded-lg bg-transparent px-3 shadow-lg lg:hidden`}
+      className={`${props.openNavbar ? 'block' : 'hidden'} sticky inset-0 z-40 mt-10 flex flex-grow flex-col items-start justify-center space-y-5 rounded-lg bg-transparent px-3 shadow-lg lg:hidden`}
     >
       {props.data?.map((item, index) => (
         <NavList key={index} title={item.title} target={item.target} href={item.href} />
       ))}
+      <ButtonDonateLanguage className='flex lg:hidden' textAlign='text-left' />
     </div>
     <React.Fragment>
       <div
@@ -62,7 +66,7 @@ const NavWrapper = (props: {
         {createElement(XMarkIcon, { className: 'h-6 w-6 text-white', strokeWidth: 4 })}
       </div>
     </React.Fragment>
-    <ButtonDonateLanguage />
+    <ButtonDonateLanguage className='hidden lg:flex' textAlign='text-right' />
   </div>
 )
 
